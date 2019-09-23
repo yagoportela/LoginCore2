8064201134da373687c77c7f1f88af616308b2f5
yago.portela <yago.portela-at-860615044000>
2019-09-23 14:24:55 -0300

    adicionando api dos dados do usuario

---
+++ UsersData.Api/UsersData.Domain/Interfaces/IRepository.cs
@@ -0,0 +1,21 @@
+﻿using UsersData.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UsersData.Domain.Interfaces
+{
+    public interface IRepository<T> where T : BaseEntity
+    {
+        void Insert(T obj);
+
+        void Update(T obj);
+
+        void Delete(int id);
+
+        T Select(int id);
+
+        IList<T> Select();
+    }
+}
+
