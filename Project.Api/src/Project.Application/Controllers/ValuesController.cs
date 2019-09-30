c9c58b98b8bb27eac6980c6a00a61ac5331c66d1
yago.portela <yago.portela-at-860615044000>
2019-09-30 13:52:49 -0300

    Ajustes no framework do MySql

--- Project.Api/src/Project.Application/Controllers/ValuesController.cs
+++ Project.Api/src/Project.Application/Controllers/ValuesController.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Net;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Project.Domain.Entities;
+using Project.Domain.Enum;
 using Project.Domain.Interfaces.Services.User;
 
 namespace Application.Controllers
@@ -26,6 +28,11 @@ namespace Application.Controllers
             
             
             try {
+                var userEntity = new UserEntity();
+                userEntity.Id = Guid.NewGuid();
+                userEntity.ImgPerfil = "aggdjsgfdsjfds.png";
+                userEntity.Tipo = typeUsers.Cliente;
+                var inserir = _user.Post(userEntity);
                 return Ok(await _user.GetAll());
             }
             catch (ArgumentException ex) {
